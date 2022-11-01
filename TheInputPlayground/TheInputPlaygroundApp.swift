1afd77756fa5f86f91803244ac4b44d0601c2001
G <guill.bout@gmail.com>
2022-11-01 19:17:43 +0800

    Initial Commit

---
+++ TheInputPlayground/TheInputPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheInputPlaygroundApp.swift
+//  TheInputPlayground
+//
+//  Created by Guillaume Leclerc on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheInputPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
